b38e4cd50a309e44b553351de339105c6c0f8f14
arpita199812 <arpitasingh199812@gmail.com>
2024-06-27 10:05:47 +0000

    adding aws project

---
+++ aws_project_using_aws_cli/aws_resource_creating_using_aws_cli.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+###############################
+#Author : Arpita Singh
+#Date : 30-May-2024
+#
+#Version : v1
+#
+#Using AWS CLI to create AWS resources using Access key and Secret Access Key of IAM User.
+#################################
+
+#Creating IAM Role to Access EC2
+echo "Print Created IAM Role"
+
+
+# Create the IAM Role
+aws iam create-role \
+    --role-name Test-Role \
+    --assume-role-policy-document file://Test-Role-Trust-Policy.json
+
+# Create the Permissions Policy
+aws iam create-policy \
+    --policy-name AmazonEC2FullAccess \
+    --policy-document file://EC2FullAccessPolicy.json
+
+# Attach the Policy to the Role (Replace the PolicyArn with the actual one returned by the previous command)
+aws iam attach-role-policy \
+    --role-name Test-Role \
+    --policy-arn arn:aws:iam::730335449419:policy/EC2FullAccessPolicy
